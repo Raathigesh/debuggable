50fa81a112e5384f7fc3c45d764ffcd08b279a27
Raathigeshan Kugarajan <raathigesh92@gmail.com>
2017-12-23 23:53:11 +1100

    New features

--- src/App.js
+++ src/App.js
@@ -21,9 +21,12 @@ const IndexPage = () => (
   <IndexGrid>
     <Header />
     <RightFeature image={require("./images/feature1.png")} header={"Test Explorer"} subheader={"Locate tests with ease through the test explorer"}/>
-    <LeftFeature image={require("./images/feature2.png")} header={"Quick Search"} subheader={"Search across your it statements"}/>
-    <RightFeature image={require("./images/feature3.png")} header={"Coverage reports"} subheader={"Integrated converage reports"}/>
-    <LeftFeature image={require("./images/feature4.png")} header={"Selective snapshot updates"} subheader={"Inline failure messages & Individual snapshot updates"}/>
+    <LeftFeature image={require("./images/feature5.png")} header={"Tests View"} subheader={"Displays It statements from the test file"}/>
+    <RightFeature image={require("./images/feature6.png")} header={"Multiple Watch Modes"} subheader={"Watch project or a file or a particular test"}/>
+    <LeftFeature image={require("./images/feature2.png")} header={"Quick Search"} subheader={"Search across all It statements"}/>
+    <RightFeature image={require("./images/feature3.png")} header={"Coverage Reports"} subheader={"Integrated converage reports"}/>
+    <LeftFeature image={require("./images/feature4.png")} header={"Selective Snapshot Update"} subheader={"Inline failure messages & Individual snapshot updates"}/>
+    <RightFeature image={require("./images/feature7.png")} header={"Inline Summary"} subheader={"Test summary and coverage summary"}/>
     <Footer />
   </IndexGrid>
 );
